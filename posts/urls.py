89c0c699c6d3226a6c1bc8dc9c776104d73d47b4
Dev-Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2020-06-29 22:41:27 +0530

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('posts/new/', views.writePost, name='new-post'),
-    path('trending/', views.postList, name='post-list'),
-    path('<slug:user>/posts/<slug:post>/', views.postDetail, name='post-detail'),
-    path('like/', views.likePost, name='like_post'),
-    path('<slug:user>/posts/', views.userPost, name='users-posts')
+    path('posts/new/', login_required(views.writePost), name='new-post'),
+    path('trending/',login_required(views.postList), name='post-list'),
+    path('<slug:user>/posts/<slug:post>/',login_required(views.postDetail), name='post-detail'),
+    path('like/',login_required(views.likePost), name='like_post'),
+    path('<slug:user>/posts/',login_required(views.userPost), name='users-posts')
 ]
