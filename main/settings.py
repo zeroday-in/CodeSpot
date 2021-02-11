6b6af368598c0edf5cfce80dcd821b4bb13bfe2d
Dev Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2021-02-11 19:16:31 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^f2(v#=b1!!+4m6@wa@%k*_()xp)1*5eta#2=^$$@0-jb)^c_f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
